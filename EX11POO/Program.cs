cbfc171ba4076b3bb866bf1a26ca873a23a51628
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-23 18:49:22 -0300

    Add files via upload

---
+++ EX11POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX11POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do lado A: ");
+            double ladoA = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            double ladoB = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            double ladoC = double.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(ladoA, ladoB, ladoC);
+            string classificacao = triangulo.ClassificarTriangulo();
+            Console.WriteLine($"Classificação do triângulo: {classificacao}");
+        }
+    }
+}
---
+++ EX11POO/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX11POO
+{
+    internal class Triangulo
+    {
+        private double ladoA, ladoB, ladoC;
+
+        public Triangulo(double a, double b, double c)
+        {
+            ladoA = a;
+            ladoB = b;
+            ladoC = c;
+        }
+
+        public string ClassificarTriangulo()
+        {
+            if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA)
+            {
+                if (ladoA == ladoB && ladoB == ladoC)
+                    return "Triângulo Equilátero";
+                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    return "Triângulo Isósceles";
+                else
+                    return "Triângulo Escaleno";
+            }
+            else
+            {
+                return "Não forma um triângulo";
+            }
+        }
+    }
+}
